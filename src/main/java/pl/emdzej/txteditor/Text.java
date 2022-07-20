5f7fe5ea31226534f7aa7ff8bb59106c2edbe9bb
Emjos <hubert.lyczko@gmail.com>
2022-07-20 16:42:32 +0100

    Initial commit

---
+++ src/main/java/pl/emdzej/txteditor/Text.java
@@ -0,0 +1,18 @@
+package pl.emdzej.txteditor;
+
+public class Text {
+
+    String text;
+
+    public Text(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
